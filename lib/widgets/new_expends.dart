27a12b318301a6d55afced80ddcae96255675aee
subexi <h_dittrich@me.com>
2026-03-15 17:32:24 +0100

    113 Adding a new input, amount

--- lib/widgets/new_expends.dart
+++ lib/widgets/new_expends.dart
@@ -10,13 +10,15 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
-  // A variable to hold the entered title of the expense
+  // A variablws to hold the entered info of the expense
   final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
 
   @override
   // Dispose of the TextEditingController when the widget is removed from the widget tree
   void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
 
@@ -27,19 +29,49 @@ class _NewExpenseState extends State<NewExpense> {
       padding: const EdgeInsets.all(16),
       child: Column(
         children: [
+          // A TextField to input the title of the expense, with a maximum length of 50 characters
           TextField(
             // Use the TextEditingController to manage the input for the title of the expense
             controller: _titleController,
             maxLength: 50,
+            // Set the keyboard type to text for the title input
+            keyboardType: TextInputType.text,
             decoration: const InputDecoration(
               label: Text('Title'),
             ),
           ),
+
+          // A TextField to input the amount of the expense
           Row(
             children: [
+              Expanded(
+                child: TextField(
+                  // Set the keyboard type to number for the amount input
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    label: Text('Amount'),
+                    // Add a prefix text to indicate that the amount is in dollars
+                    prefixText: '\$ ',
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+
+          Row(
+            children: [
+              TextButton(
+                onPressed: () {},
+                  child: const Text(
+                    'Cancel',
+                  ),
+                ),
               ElevatedButton(
                 onPressed: () {
                   print(_titleController.text);
+                  print(_amountController.text);
                 },
                 child: const Text(
                   'Save Expense',
